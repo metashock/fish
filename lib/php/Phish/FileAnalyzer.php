45ba92c34570b9d58cbdb2d81a08d4bfcb8e866d
Thorsten Heymann <info@metashock.net>
2013-10-30 23:01:22 +0100

    Added `parseComment()` to `FileAnalyzer_PHP`

--- lib/php/Phish/FileAnalyzer.php
+++ lib/php/Phish/FileAnalyzer.php
@@ -120,5 +120,41 @@ class Phish_FileAnalyzer
         $str .= '  ' . implode(PHP_EOL . "  ", $this->classes());
         return $str;
     }
+
+
+
+    /**
+     *
+     */
+    public static function parseComment($string) {
+
+        //$returnPattern = "/^[ \t]*\*[ \t]*\@return[ \t]*[a-bA-B_]+[a-bA-B_0-9]?[ \t]?$/";
+
+        $returnPattern = "/@return[ \t]*([a-zA-Z_]+[a-zA-Z0-9_]?)[ \t]?/";
+        $paramPattern = "/@param[ \t]*([a-zA-Z_]+[a-zA-Z0-9_]?)[ \t]*.([a-zA-Z_]+[a-zA-Z0-9_]?)[ \t]?/";
+
+        $meta = array(
+            'returnType' => '?',
+            'params' => array()
+        );
+
+        foreach(explode(PHP_EOL, $string) as $line) {
+            if(preg_match($returnPattern, $line, $matches)) {
+                $meta['returnType'] = $matches[1];
+                continue;
+            }
+            if(preg_match($paramPattern, $line, $matches)) {
+                $meta['params'][$matches[2]] = array(
+                    'type' => $matches[1],
+                    'name' => $matches[2]
+                );
+            }
+        }
+
+        $meta['text'] = '';
+        return $meta;
+    }
+
+
 }
 
